1794e4000f67290d4db08fb9c59bddb10032e36f
nosk <liath.fail.org@gmail.com>
2021-09-02 21:39:38 +0900

    envファイルを追加

--- front/src/main.js
+++ front/src/main.js
@@ -6,14 +6,7 @@ import axios from "axios";
 
 import "@/assets/css/font.css";
 
-
-if (process.env.NODE_ENV == "production") {
-  axios.defaults.baseURL =  "https://3b.intern.jigd.info";
-} else if (process.env.NODE_ENV == "local") {
-  axios.defaults.baseURL =  "http://localhost:8891";
-}
-
-// axios.defaults.baseURL = "https://3b.intern.jigd.info";
+axios.defaults.baseURL =  process.env.VUE_APP_API_BASE_URL;
 
 createApp(App)
   .use(store)
